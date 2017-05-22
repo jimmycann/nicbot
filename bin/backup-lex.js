0332204987d064fb5cb720cce5f34b8682e8ed3e
Jimmy Cann <mail@jimmycann.com>
2017-05-23 09:02:19 +1000

    Fix Lex backup scripts, add method for pulling Slots and Intents

--- bin/backup-lex.js
+++ bin/backup-lex.js
@@ -11,28 +11,51 @@ Bluebird.promisifyAll(fs);
 
 const AWS = require('aws-sdk');
 AWS.config.update({
-  region: 'ap-southeast-2',
+  region: 'us-east-1',
   accessKeyId: process.env.NICBOT_AWS_ACCESS_KEY_ID,
   secretAccessKey: process.env.NICBOT_AWS_SECRET_ACCESS_KEY
 });
+AWS.config.setPromisesDependency(Bluebird);
 const Lex = new AWS.LexModelBuildingService();
 
 const options = {
   LexDir: '../lex',
-  name: process.argv[2] || 'nicbot',
+  name: process.argv[2] || 'NicBot',
   versionOrAlias: process.argv[3] || '$LATEST'
 };
 
-Lex.getBot({
-  name: options.name,
-  versionOrAlias: options.versionOrAlias
-}).promise()
-  .then(bot => {
-    if (!fs.existsSync(path.join(__dirname, options.LexDir))) {
-      fs.mkdir(path.join(__dirname, options.LexDir));
-    }
-    return fs.writeFileAsync(path.join(__dirname, `${options.LexDir}/${options.name}-${options.versionOrAlias}-bkup.json`), JSON.stringify(bot, null, 2));
-  })
+const writeBackup = function (resource, newFile) {
+  return fs.writeFileAsync(path.join(__dirname, newFile), JSON.stringify(resource, null, 2));
+};
+
+const getBot = function () {
+  return Lex.getBot({
+    name: options.name,
+    versionOrAlias: options.versionOrAlias
+  }).promise()
+    .then(bot => writeBackup(bot, `${options.LexDir}/bots/${options.name}-${options.versionOrAlias}-bkup.json`));
+};
+
+const getIntents = function () {
+  return Lex.getIntents({
+    maxResults: 50
+  }).promise()
+    .then(bot => writeBackup(bot, `${options.LexDir}/intents/${options.name}-${options.versionOrAlias}-bkup.json`));
+};
+
+const getSlotTypes = function () {
+  return Lex.getSlotTypes({
+    maxResults: 50
+  }).promise()
+    .then(bot => writeBackup(bot, `${options.LexDir}/slots/${options.name}-${options.versionOrAlias}-bkup.json`));
+};
+
+Bluebird.all([
+  getBot(),
+  getIntents(),
+  getSlotTypes()
+])
   .catch(err => {
-    throw new Error('Unable to complete backup', err);
+    console.warn(err);
+    process.exit();
   });
