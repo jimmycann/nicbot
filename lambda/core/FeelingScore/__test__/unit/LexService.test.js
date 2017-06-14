a196ec7a0ab33439ce8a29458da2622d53f14897
Jimmy Cann <mail@jimmycann.com>
2017-06-14 22:21:45 +1000

    Update FeelingScore tests

--- lambda/core/FeelingScore/__test__/unit/LexService.test.js
+++ lambda/core/FeelingScore/__test__/unit/LexService.test.js
@@ -1,8 +1,5 @@
-const LexFixture = require('../fixtures/LexFixture');
-
 const LexService = require('../../service/LexService');
 const sinon = require('sinon');
-const Bluebird = require('bluebird');
 
 let sandbox;
 
@@ -17,18 +14,5 @@ describe('#LexService', () => {
     it('should fail if the nextAction is null', () => {
       return LexService.buildResponse('', null).should.be.rejectedWith('nextAction is required');
     });
-  });
-
-  describe('#Business Logic', () => {
-    // it('should succeed and return a valid response object', () => {
-    //   const session = {
-    //     completedDistractions: LexFixture.newCompletedArray()
-    //   };
-    //   const nextAction = LexFixture.newDistractionObj();
-    //
-    //   return Bluebird.resolve()
-    //     .then(() => LexService.buildResponse(session, nextAction))
-    //     .then(res => LexFixture.validate(res));
-    // });
   });
 });
